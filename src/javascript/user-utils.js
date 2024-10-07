1f3ee7b92a606d31f64eced327953893605576f4
Luiz Henrique Firmino de Jesus <l176204@dac.unicamp.br>
2024-10-07 14:05:56 -0300

    👤 Implement user account registration logic

---
+++ src/javascript/user-utils.js
@@ -0,0 +1,30 @@
+'use strict';
+
+export function getCurrentUser() {
+    return JSON.parse(localStorage.getItem('currentUser'));
+}
+
+export function setCurrentUser(user) {
+    localStorage.setItem('currentUser', JSON.stringify(user));
+}
+
+export function removeCurrentUser() {
+    localStorage.removeItem('currentUser');
+}
+
+export function getUsers() {
+    return JSON.parse(localStorage.getItem('users')) || [];
+}
+
+export function saveUsers(users) {
+    localStorage.setItem('users', JSON.stringify(users));
+}
+
+export function findUser(cpfOrEmailOrUsername) {
+    const users = getUsers();
+    return users.find(user => 
+        user.cpf === cpfOrEmailOrUsername || 
+        user.username === cpfOrEmailOrUsername ||
+        user.email === cpfOrEmailOrUsername
+    );
+}
